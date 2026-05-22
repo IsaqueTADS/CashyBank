80e456013ddb57f2c059a8d9809c51d7f734bf3f
IsaqueTADS <isaqueofc18@gmail.com>
2026-05-22 08:13:30 -0300

    feat: add get transaction types and categories use case

---
+++ src/use-cases/get-transaction-data-use-case.ts
@@ -0,0 +1,22 @@
+import type { TransactionCategory, TransactionType } from '@/dtos/transaction/transaction.js'
+import { prisma } from '@/lib/prisma.js'
+
+interface GetTransactionDataUseCaseResponse {
+  types: TransactionType[]
+  categories: TransactionCategory[]
+}
+
+export class GetTransactionDataUseCase {
+  async execute(): Promise<GetTransactionDataUseCaseResponse> {
+    const [types, categories] = await Promise.all([
+      prisma.transactionType.findMany({
+        select: { id: true, name: true },
+      }),
+      prisma.transactionCategory.findMany({
+        select: { id: true, name: true },
+      }),
+    ])
+
+    return { types, categories }
+  }
+}
