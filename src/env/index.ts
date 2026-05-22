81a1d4cb9a11cbb335eeb0a8b57e9ed13ac841e2
IsaqueTADS <isaqueofc18@gmail.com>
2026-05-22 08:13:30 -0300

    feat: add env validation with zod

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+import z from 'zod'
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  API_URL: z.string().default('http://localhost:3333'),
+  JWT_SECRET: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.error('Invalid environment variables')
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
